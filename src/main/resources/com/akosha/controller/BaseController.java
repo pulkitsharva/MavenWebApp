0d7f217dd944d968740dc961cb1b13ef4a6c2db4
pulkitsharva <pulkit.sharva@gmail.com>
2014-09-06 18:39:11 +0530

    added restful service implementation

--- src/main/resources/com/akosha/controller/BaseController.java
+++ src/main/resources/com/akosha/controller/BaseController.java
@@ -2,6 +2,7 @@ package com.akosha.controller;
  
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
  
@@ -9,22 +10,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class BaseController {
  
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String welcome(ModelMap model) {
+	@RequestMapping(value="/welcome/{name}", method = RequestMethod.GET)
+	public String welcome(@PathVariable String name,ModelMap model) {
  
-		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome() "+name);
  
 		//Spring uses InternalResourceViewResolver and return back index.jsp
 		return "index";
  
 	}
  
-//	@RequestMapping(value="/welcome/{name}", method = RequestMethod.GET)
-//	public String welcomeName(@PathVariable String name, ModelMap model) {
-// 
-//		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - " + name);
-//		return "index";
-// 
-//	}
+	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	public String welcomeName( ModelMap model) {
+ 
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - ");
+		return "index";
+ 
+	}
  
 }
